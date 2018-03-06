e7717f6ec2215f124dbbb5fbef9315b41b544758
Emily Satterfield <esatterfield32@gmail.com>
2018-03-06 16:47:17 -0500

    add leds

--- chairmansPresentation5962.ino
+++ chairmansPresentation5962.ino
@@ -1,14 +1,24 @@
+#include <FastLED.h>
 int piston1= 2;
 int piston2= 3;
 int piston3= 4;
 int piston4= 5;
-int piston5= 6;
 int incomingByte = 0;
+
+#define NUM_LEDS 56
+
+#define LED_DATA_PIN 10
+
+CRGB leds[NUM_LEDS];
+CRGB blink_color; // saved color
+CRGB snapshot_colors[] = {CRGB::White,CRGB::Blue,CRGB::Red,CRGB::Yellow,CRGB::Green};
 
 void setup() {
    Serial.begin(9600);
-   for(int i = 2;  i<7; i++){
+   FastLED.addLeds<WS2812B, LED_DATA_PIN, GRB>(leds, NUM_LEDS);
+   for(int i = 2;  i<6; i++){
     pinMode(i,OUTPUT);
+    
    }
    
  
@@ -16,9 +26,11 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
+ 
   if (Serial.available()>0){
    incomingByte = Serial.read();
    Serial.println("recieved command" + (char)incomingByte);
+  
    if (incomingByte == '1'){
     powerPiston(piston1);
    }
@@ -31,24 +43,34 @@ void loop() {
     else if (incomingByte == '4'){
     powerPiston(piston4);
    }
-    else if (incomingByte == '5'){
-    powerPiston(piston5);
-   }
-    else if (incomingByte == '6'){
-    
-   }
    
 }
 }
 
-void powerPiston(int pistonNumber){
-  if(digitalRead(pistonNumber) == 1){
+void powerPiston(int pistonNumber ){
+  int startNumber = (pistonNumber - 2) * 14;
+  int endNumber = (pistonNumber - 1) * 14;
+ 
+  if(digitalRead(pistonNumber) == 1){  
+   
+    Serial.println(startNumber);
+    Serial.println(endNumber);
+    for (int led = startNumber; led < endNumber; led++) {
+    leds[led] = CRGB::Yellow;
+    FastLED.show();
+    }
     digitalWrite(pistonNumber, LOW);
     Serial.println("relay on");
-  }
+    
+    } 
+  
   else{
     digitalWrite(pistonNumber, HIGH);
     Serial.print("relay off");
+    for (int led = startNumber; led < endNumber; led++) {
+    leds[led] = CRGB::Black;
+    FastLED.show();
+    }
   }
 }
 
